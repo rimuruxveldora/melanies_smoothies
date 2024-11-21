c2cb8d58321a900ff03ba02cc351b02de9d50cc0
rimuruxveldora <jannronellecondestable@gmail.com>
2024-11-21 11:08:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,93 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 # st.write("You selected:", option)
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col
+ 
+# Write directly to the app
+st.title(":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
+st.write(
+    """Choose the fruits you want in your custom Smoothie!
+    """
+)
+ 
+name_on_order = st.text_input("Name on Smoothie: ")
+st.write("The name on your Smoothie will be: ", name_on_order)
+ 
+# option = st.selectbox(
+#     "What is your favorite fruit?",
+#     ("Banana", "Strawberry", "Peaches"),
+# )
+ 
+# st.write("You selected:", option)
+cnx = st.connection("snowflake")
+session = cnx.session()
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col
+ 
+# Write directly to the app
+st.title(":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
+st.write(
+    """Choose the fruits you want in your custom Smoothie!
+    """
+)
+ 
+name_on_order = st.text_input("Name on Smoothie: ")
+st.write("The name on your Smoothie will be: ", name_on_order)
+ 
+# option = st.selectbox(
+#     "What is your favorite fruit?",
+#     ("Banana", "Strawberry", "Peaches"),
+# )
+ 
+# st.write("You selected:", option)
+cnx = st.connection("snowflake")
+session = cnx.session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
+ 
+ingredients_list = st.multiselect(
+    "Choose up to 5 ingredients: "
+    , my_dataframe
+    , max_selections=5
+)
+ 
+if ingredients_list:
+ 
+    ingredients_string = ''
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+ 
+    time_to_insert = st.button('Submit Order')
+ 
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success("Your Smoothie is ordered, " + name_on_order + "!", icon="✅")
+ 
+ingredients_list = st.multiselect(
+    "Choose up to 5 ingredients: "
+    , my_dataframe
+    , max_selections=5
+)
+ 
+if ingredients_list:
+ 
+    ingredients_string = ''
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+ 
+    time_to_insert = st.button('Submit Order')
+ 
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success("Your Smoothie is ordered, " + name_on_order + "!", icon="✅")
  
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients: "
